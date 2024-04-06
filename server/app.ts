ca407bdd70b49b8f4629c2fd2f796b6a5d9bba21
AnjezaSfishta <anjeza.sfishta@student.uni-pr.edu>
2024-04-06 12:15:15 +0200

    update registration

--- server/app.ts
+++ server/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import { Request, Response } from 'express';
-
+import userRouter from "./routes/user.route";
 
 //body parser
 
@@ -18,7 +18,12 @@ app.use(cors({
 origin:process.env.ORIGIN
 }));
 
-
+// routes
+app.use(
+    "/api/v1",
+    userRouter
+  );
+  
 //testing api
 
 app.get("/test",(req:Request,res:Response,next:NextFunction)=>{
