562a7b26dbfe4aee35b76babd91f459ce88fe5a8
triinga <97124034+Triinga@users.noreply.github.com>
2024-04-07 15:28:07 +0200

    Login form added

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,62 +1,78 @@
 import React, { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import NavItems from "../utils/NavItems";
-import {HiOutlineUserCircle} from "react-icons/hi";
-import { ThemeSwitcher} from "../utils/ThemeSwitcher";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { ThemeSwitcher } from "../utils/ThemeSwitcher";
+import CustomModal from "../utils/CustomModal";
+import Login from "../components/Auth/Login";
 
 type Props = {
-    open: boolean;
-    setOpen: (open:boolean) => void;
-    activeItem: number;
-}
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  activeItem: number;
+  route: string;
+  setRoute: (route: string) => void;
+};
 
-const Header: FC<Props> = ({activeItem,setOpen}) => {
-    const [active, setActive] = useState(false);
-    const [openSidebar, setOpenSidebar] = useState(false);
+const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
+  const [active, setActive] = useState(false);
+  const [openSidebar, setOpenSidebar] = useState(false);
 
-    if (typeof window !== "undefined") {
-        window.addEventListener("scroll", () => {
-        if (window.scrollY > 85) {
-            setActive(true);
-        } else {
-            setActive(false);
-        }
-        });
-    }
-return (
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 85) {
+        setActive(true);
+      } else {
+        setActive(false);
+      }
+    });
+  }
+  return (
     <div className="w-full relative">
-    <div
-    className={`${
-        active
-        ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
-        : "w-full border-b dark:border-[#4235351c] h-[80px] z-[80] dark:shadow"
-    }`}
-    >
-    <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
-    <div className="w-full h-[80px] flex items-center justify-between p-3">
-        <div>
-        <Link
-            href={"/"}
-            className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
-        >
-            Elearning
-        </Link>
-        </div>
-        <div className="flex items-center">
-            <NavItems activeItem={activeItem} isMobile={false} />
-            <ThemeSwitcher />
-            <HiOutlineUserCircle
+      <div
+        className={`${
+          active
+            ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+            : "w-full border-b dark:border-[#4235351c] h-[80px] z-[80] dark:shadow"
+        }`}
+      >
+        <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+          <div className="w-full h-[80px] flex items-center justify-between p-3">
+            <div>
+              <Link
+                href={"/"}
+                className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+              >
+                Elearning
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <NavItems activeItem={activeItem} isMobile={false} />
+              <ThemeSwitcher />
+              <HiOutlineUserCircle
                 size={25}
                 className="hidden 800px:block cursor-pointer dark:text-white text-black"
                 onClick={() => setOpen(true)}
-                />
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {route === "Login" && (
+        <>
+          {open && (
+            <CustomModal
+              open={open}
+              setOpen={setOpen}
+              setRoute={setRoute}
+              activeItem={activeItem}
+              component={Login}
+            />
+          )}
+        </>
+      )}
     </div>
-
-    </div>
-    </div>
-    </div>
-    </div>
-);
+  );
 };
 
-export default Header
+export default Header;
