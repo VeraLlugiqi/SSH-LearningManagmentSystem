ffb335b47c2539226181f104d1e559ce4f9bcc90
triinga <97124034+Triinga@users.noreply.github.com>
2024-04-21 12:22:43 +0200

    added question and reply to a course feature
    To be added is a notification that a question is added and review for course

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadCourse, editCourse ,getSingleCourse, getAllCourses,getCourseByUser} from "../controllers/course.controller";
+import { uploadCourse, editCourse ,getSingleCourse, getAllCourses,getCourseByUser, addQuestion, addAnswer} from "../controllers/course.controller";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -22,6 +22,14 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 
 courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
+courseRouter.put(
+    "/add-question", 
+    isAutheticated, 
+    addQuestion);
 
+courseRouter.put(
+    "/add-answer", 
+    isAutheticated, 
+    addAnswer);
 
 export default courseRouter;
