502b22310d8bdb313fc2c54e28ec63f7c2403cdb
Rinesa Zuzaku <rinesazuzaku@gmail.com>
2024-05-03 23:54:50 +0200

    LMS:19: Added review in Course

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -243,6 +243,11 @@ export const updateAccessToken = CatchAsyncError(
       res.cookie("access_token", accessToken, accessTokenOptions);
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
 
+      // res.status(200).json({
+      //   status:"success",
+      //   accessToken,
+      // })
+
       await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7days
 
       return next();
