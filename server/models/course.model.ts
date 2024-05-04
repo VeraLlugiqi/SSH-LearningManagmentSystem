5c21e3408b1cb238c3ae2465d6912faead2d54f5
triinga <97124034+Triinga@users.noreply.github.com>
2024-05-04 22:14:02 +0200

    LMS-20: order and notification models, order controller and service
    now you can order (in order to do this you must be authenticated). When you order a course, you will receive a confirmation order in your inbox

--- server/models/course.model.ts
+++ server/models/course.model.ts
@@ -47,7 +47,7 @@ interface ICourse extends Document{
     reviews: IReview[];
     courseData: ICourseData[];
     ratings?: number;
-    purchased?: number;
+    purchased: number;
 }
 
 const reviewSchema = new Schema<IReview>({
