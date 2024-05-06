3c4e8efe4ed96a7334a10fba5a2c03a74c333f96
AnjezaSfishta <anjeza.sfishta@student.uni-pr.edu>
2024-05-06 19:32:47 +0200

    create analystics generator
    Get the last 28 days' users,orders, notifications for the last 1year

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,7 @@ import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
+import analyticsRouter from "./routes/analytics.route";
 //body parser
 
 app.use(express.json({limit:"50mb"}));
@@ -24,7 +25,7 @@ credentials:true
 // routes
 app.use(
     "/api/v1",
-    userRouter, orderRouter, courseRouter,notificationRouter
+    userRouter, orderRouter, courseRouter,notificationRouter,analyticsRouter
     
 );
 //testing api
